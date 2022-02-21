eb6ae48f275b8d4e1ec6722f9608efad2f5e2510
Selahattin Güngör <67766746+SelahattinGngr@users.noreply.github.com>
2022-02-21 23:11:30 +0300

    Add files via upload

---
+++ 005 fonksiyon egzersizleri/001.iki_sayinin_toplami/iki_sayi_toplam.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void	iki_sayi_toplam(int a,int b)
+{
+	printf("%d + %d = %d",a,b,a+b);
+}
---
+++ 005 fonksiyon egzersizleri/001.iki_sayinin_toplami/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void	iki_sayi_toplam(int a,int b);
+
+int	main()
+{
+	int	a,b;
+	printf("Lutfen 2 tane tam sayi giriniz\n");
+	scanf("%d%d",&a,&b);
+	iki_sayi_toplam(a,b);
+}
